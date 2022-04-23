f2377ec0d0bd299917679f560d2c628574d5fe16
davidr3489@gmail.com <crowden3489>
2022-04-23 13:55:36 +0200

    Removed unnecessary print function

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,8 +5,6 @@ nato_data_frame = pandas.DataFrame(data)
 
 phonetic_dictionary = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
-print(phonetic_dictionary)
-
 word = input("Enter a word: ").upper()
 word_split = [phonetic_dictionary[letter] for letter in word]
 
