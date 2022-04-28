5f88ca7ac00dfc4d53a9850b1bdd03f51a9f440f
davidr3489@gmail.com <crowden3489>
2022-04-28 18:06:25 +0200

    Added error exception to loop back if characters other than letters are used.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,7 +5,14 @@ nato_data_frame = pandas.DataFrame(data)
 
 phonetic_dictionary = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
-word = input("Enter a word: ").upper()
-word_split = [phonetic_dictionary[letter] for letter in word]
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_split = [phonetic_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Invalid entry. Use letters in the alphabet only.")
+        generate_phonetic()
+    else:
+        print(word_split)
 
-print(word_split)
+generate_phonetic()
